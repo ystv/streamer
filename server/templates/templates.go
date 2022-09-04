9e5213f49ff2008fb876f441aea5389799a4f0a8
Liam Burnand <liamb1216@gmail.com>
2022-09-04 07:40:19 +0100

    Changing from static html pages to templates, or at least trying to

---
+++ server/templates/templates.go
@@ -0,0 +1,46 @@
+package templates
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"time"
+)
+
+//go:embed *.tmpl
+var tpls embed.FS
+
+type (
+	Templater struct {
+		dashboard *template.Template
+	}
+	BaseParams struct {
+		SystemTime time.Time
+	}
+	DashboardParams struct {
+		Base BaseParams
+	}
+)
+
+var funcs = template.FuncMap{
+	"cleantime": cleanTime,
+}
+
+func cleanTime(t time.Time) string {
+	return t.Format(time.RFC1123Z)
+}
+
+func parse(file string) *template.Template {
+	return template.Must(
+		template.New("base.tmpl").Funcs(funcs).ParseFS(tpls, "base.tmpl", file))
+}
+
+func New() *Templater {
+	return &Templater{
+		dashboard: parse("dashboard.tmpl"),
+	}
+}
+
+func (t *Templater) Dashboard(w io.Writer, p DashboardParams) error {
+	return t.dashboard.Execute(w, p)
+}
