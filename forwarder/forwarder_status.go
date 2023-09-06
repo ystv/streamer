2121d9ba7527364a560558321984aa2d4bd46879
Liam Burnand <liam.burnand@ystv.co.uk>
2023-09-06 23:02:07 +0100

    Moved forwarder status from main to status function

--- forwarder/forwarder_status.go
+++ forwarder/forwarder_status.go
@@ -1,32 +1,63 @@
 package main
 
 import (
+	"bufio"
+	_ "embed"
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	if strings.Contains(os.Args[0], "/var/folders") || strings.Contains(os.Args[0], "/tmp/go") || strings.Contains(os.Args[0], "forwarder_status") {
-		if len(os.Args) != 4 {
-			log.Fatalf("Arguments error")
-		}
-		for i := 0; i < len(os.Args)-1; i++ {
-			os.Args[i] = os.Args[i+1]
-		}
-	} else {
-		if len(os.Args) != 3 {
-			log.Fatalf("Arguments error")
-		}
-	}
+//go:embed forwarder_status.sh
+var statusScript string
 
-	website, _ := strconv.ParseBool(os.Args[0])
-	streams, _ := strconv.Atoi(os.Args[1])
-	unique := os.Args[2]
+//func main() {
+//	if strings.Contains(os.Args[0], "/var/folders") || strings.Contains(os.Args[0], "/tmp/go") || strings.Contains(os.Args[0], "forwarder_status") {
+//		if len(os.Args) != 4 {
+//			log.Fatalf("Arguments error")
+//		}
+//		for i := 0; i < len(os.Args)-1; i++ {
+//			os.Args[i] = os.Args[i+1]
+//		}
+//	} else {
+//		if len(os.Args) != 3 {
+//			log.Fatalf("Arguments error")
+//		}
+//	}
+//
+//	website, _ := strconv.ParseBool(os.Args[0])
+//	streams, _ := strconv.Atoi(os.Args[1])
+//	unique := os.Args[2]
+//
+//	var start int
+//
+//	if website {
+//		start = 0
+//	} else {
+//		start = 1
+//	}
+//
+//	m := make(map[string]string)
+//
+//	for i := start; i <= streams; i++ {
+//		out, err := exec.Command("./forwarder_status.sh", unique, strconv.Itoa(i)).Output()
+//		if err != nil {
+//			log.Fatal(err.Error())
+//		} else {
+//			if i == 0 {
+//				m["website~"] = "~" + string(append(out, '\u0000'))
+//			} else {
+//				m[strconv.Itoa(i)+"~"] = "~" + string(append(out, '\u0000'))
+//			}
+//		}
+//	}
+//
+//	fmt.Println(m)
+//}
 
+func status(unique string, website bool, streams int) {
 	var start int
 
 	if website {
@@ -38,14 +69,26 @@ func main() {
 	m := make(map[string]string)
 
 	for i := start; i <= streams; i++ {
-		out, err := exec.Command("./forwarder_status.sh", unique, strconv.Itoa(i)).Output()
+		c := exec.Command("bash", "-s", "-", unique, strconv.Itoa(i), "|", "bash")
+		c.Stdin = strings.NewReader(statusScript)
+
+		stderr, _ := c.StderrPipe()
+		b, err := c.Output()
+		if err != nil {
+			log.Fatalf("echo %+v", err)
+		}
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			log.Fatalf("echo %s", scanner.Text())
+		}
+
 		if err != nil {
 			log.Fatal(err.Error())
 		} else {
 			if i == 0 {
-				m["website~"] = "~" + string(append(out, '\u0000'))
+				m["website~"] = "~" + string(append(b, '\u0000'))
 			} else {
-				m[strconv.Itoa(i)+"~"] = "~" + string(append(out, '\u0000'))
+				m[strconv.Itoa(i)+"~"] = "~" + string(append(b, '\u0000'))
 			}
 		}
 	}
