ce15fe1d091afcea7109a4730c792123b9228a18
Liam Burnand <liam.burnand@ystv.co.uk>
2023-09-06 23:02:24 +0100

    Moved forwarder stop from main to stop function

--- forwarder/forwarder_stop.go
+++ forwarder/forwarder_stop.go
@@ -6,33 +6,17 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
-func main() {
-	if strings.Contains(os.Args[0], "/var/folders") || strings.Contains(os.Args[0], "/tmp/go") || strings.Contains(os.Args[0], "./forwarder_stop") {
-		if len(os.Args) != 2 {
-			fmt.Println("echo", os.Args)
-			log.Fatalf("echo Arguments error")
-		}
-		for i := 0; i < len(os.Args)-1; i++ {
-			os.Args[i] = os.Args[i+1]
-		}
-	} else {
-		if len(os.Args) != 1 {
-			fmt.Println("echo", os.Args)
-			log.Fatalf("echo Arguments error")
-		}
-	}
-	unique := os.Args[0]
+func stop(unique string) {
 	gomux.KillSession("STREAM FORWARDER - "+unique, os.Stdout)
 	files, err := filepath.Glob("logs/" + unique + "_*")
 	if err != nil {
-		panic(err)
+		log.Fatalf("echo %+v", err)
 	}
 	for _, f := range files {
 		if err := os.Remove(f); err != nil {
-			panic(err)
+			log.Fatalf("echo %+v", err)
 		}
 	}
 	fmt.Println("echo FORWARDER STOPPED!")
