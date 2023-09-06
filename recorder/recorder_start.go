bfcfb187668fccf663bcb6570d01ff54de3746b3
Liam Burnand <liam.burnand@ystv.co.uk>
2023-09-06 23:01:15 +0100

    Moved recorder start from main to start function

--- recorder/recorder_start.go
+++ recorder/recorder_start.go
@@ -1,97 +1,86 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
-	"github.com/joho/godotenv"
 	"github.com/wricardo/gomux"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
-func main() {
-	fmt.Println("echo", os.Args)
-	if strings.Contains(os.Args[0], "/var/folders") || strings.Contains(os.Args[0], "/tmp/go") || strings.Contains(os.Args[0], "./recorder_start") {
-		if len(os.Args) != 4 {
-			fmt.Println("echo " + string(rune(len(os.Args))))
-			log.Fatalf("echo Arguments error")
-		}
-		for i := 0; i < len(os.Args)-1; i++ {
-			os.Args[i] = os.Args[i+1]
-		}
-	} else {
-		if len(os.Args) != 3 {
-			fmt.Println("echo " + string(rune(len(os.Args))))
-			log.Fatalf("echo Arguments error")
-		}
-	}
-	streamIn := os.Args[0]
-	pathOut := os.Args[1]
-	unique := os.Args[2]
+//go:embed recorder_start.sh
+var startScript string
+
+func start(unique, streamIn, pathOut, recordingLocation, streamServer string) {
 	array := strings.Split(pathOut, "/")
 	valid := false
 	var path string
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Printf("echo Error loading .env file: %s", err)
+
+	if len(array) == 1 {
+		path = array[0]
+		valid = true
 	} else {
-		recordingLocation := os.Getenv("RECORDING_LOCATION")
-		if len(array) == 1 {
-			path = array[0]
+		for i := 0; i < len(array)-1; i++ {
+			path += array[i] + "/"
+		}
+		err := os.MkdirAll(recordingLocation+path, 0777)
+		if err != nil {
+			fmt.Println("echo " + path)
+			fmt.Println("echo " + err.Error())
+			log.Fatal("echo Error creating " + recordingLocation + path)
+		}
+		_, err1 := os.Stat(recordingLocation + path)
+		if os.IsNotExist(err1) {
+			log.Fatal(" echo RECORDER UNSUCCESSFUL!")
+		}
+		temp := array[len(array)-1]
+		_, err2 := os.Stat(recordingLocation + path + "/" + temp)
+		if os.IsNotExist(err2) {
+			path += array[len(array)-1]
 			valid = true
 		} else {
-			for i := 0; i < len(array)-1; i++ {
-				path += array[i] + "/"
-			}
-			err = os.MkdirAll(recordingLocation+path, 0777)
-			if err != nil {
-				fmt.Println("echo " + path)
-				fmt.Println("echo " + err.Error())
-				log.Fatal("echo Error creating " + recordingLocation + path)
-			}
-			_, err1 := os.Stat(recordingLocation + path)
-			if os.IsNotExist(err1) {
-				fmt.Println(" echo RECORDER UNSUCCESSFUL!")
-			} else {
-				temp := array[len(array)-1]
-				_, err2 := os.Stat(recordingLocation + path + "/" + temp)
-				if os.IsNotExist(err2) {
-					path += array[len(array)-1]
+			split := strings.Split(temp, ".")
+			loop := true
+			i := 0
+			for loop {
+				_, err3 := os.Stat(recordingLocation + path + "/" + split[0] + "_" + string(rune(i)) + ".mkv")
+				if os.IsNotExist(err3) {
+					path += split[0] + string(rune(i)) + ".mkv"
+					loop = false
 					valid = true
-				} else {
-					split := strings.Split(temp, ".")
-					loop := true
-					i := 0
-					for loop {
-						_, err3 := os.Stat(recordingLocation + path + "/" + split[0] + "_" + string(rune(i)) + ".mkv")
-						if os.IsNotExist(err3) {
-							path += split[0] + string(rune(i)) + ".mkv"
-							loop = false
-							valid = true
-						} else {
-							i++
-						}
-					}
+					break
 				}
+				i++
 			}
-		}
-		if valid {
-			streamServer := os.Getenv("STREAM_SERVER")
-
-			sessionName := "STREAM RECORDING - " + unique
-
-			s := gomux.NewSession(sessionName, os.Stdout)
-
-			w1 := s.AddWindow("RECORDING")
-
-			w1p0 := w1.Pane(0)
-
-			path = strings.ReplaceAll(path, ".mkv", "")
-			w1p0.Exec("./recorder_start.sh " + streamServer + streamIn + " \"" + recordingLocation + path + "\" " + unique + " | bash")
-
-			fmt.Println("echo RECORDER STARTED!")
-		} else {
-			log.Fatal("echo Invalid string")
 		}
 	}
+	if !valid {
+		log.Fatal("echo Invalid string")
+	}
+	sessionName := "STREAM RECORDING - " + unique
+
+	s := gomux.NewSession(sessionName, os.Stdout)
+
+	w1 := s.AddWindow("RECORDING")
+
+	w1p0 := w1.Pane(0)
+
+	path = strings.ReplaceAll(path, ".mkv", "")
+
+	c := exec.Command("sh", "-s", "-", streamServer+streamIn, fmt.Sprintf("\"%s%s\"", recordingLocation, path), unique, "|", "bash")
+
+	c.Stdin = strings.NewReader(startScript)
+
+	b, e := c.Output()
+	if e != nil {
+		fmt.Println(e)
+	}
+	fmt.Println(string(b))
+
+	w1p0.Exec("./recorder_start.sh " + streamServer + streamIn + " \"" + recordingLocation + path + "\" " + unique + " | bash")
+
+	fmt.Println("echo RECORDER STARTED!")
+
 }
