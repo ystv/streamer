859874313f0a1c9e2d2ef0636b1990e51bf09680
Liam Burnand <liam.burnand@ystv.co.uk>
2023-09-14 23:24:35 +0100

    Updated recorder_stop.go to use the channels

--- recorder/recorder_stop.go
+++ recorder/recorder_stop.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/comtop1/gomux"
-	"log"
-	"os"
-	"os/exec"
 )
 
-func stop(unique string) {
-	gomux.KillSession("STREAM RECORDING - "+unique, os.Stdout)
-	file := "logs/" + unique + ".txt"
-	cmd := exec.Command("/bin/rm", file)
-	_, err := cmd.Output()
-	if err != nil {
-		log.Fatalf("echo %+v", err)
+func (v *Views) stop(transporter Transporter) error {
+	finish, ok := v.cache.Get(transporter.Unique + "Finish")
+	if !ok {
+		return fmt.Errorf("unable to find channel: %s", transporter.Unique)
 	}
-	fmt.Println("echo RECORDER STOPPED!")
+	close(finish.(chan struct{}))
+	return nil
 }
