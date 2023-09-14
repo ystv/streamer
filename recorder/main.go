3f06916e5f5dc6cdbaaf6ea1f92279ccd506bc9c
Liam Burnand <liam.burnand@ystv.co.uk>
2023-09-14 23:26:24 +0100

    Not needed

--- recorder/main.go
+++ recorder/main.go
@@ -24,17 +24,6 @@ type (
 		Payload interface{} `json:"payload"`
 	}
 
-	ForwarderStart struct {
-		StreamIn   string            `json:"streamIn"`
-		WebsiteOut string            `json:"websiteOut"`
-		Streams    map[string]string `json:"streams"`
-	}
-
-	ForwarderStatus struct {
-		Website bool `json:"website"`
-		Streams int  `json:"streams"`
-	}
-
 	RecorderStart struct {
 		StreamIn string `json:"streamIn"`
 		PathOut  string `json:"pathOut"`
@@ -49,35 +38,6 @@ type (
 )
 
 func main() {
-	//fmt.Println("echo", os.Args)
-	//if strings.Contains(os.Args[0], "/var/folders") || strings.Contains(os.Args[0], "/tmp/go") || strings.Contains(os.Args[0], "./recorder_start") {
-	//	if len(os.Args) != 7 && len(os.Args) != 3 {
-	//		fmt.Println("echo " + string(rune(len(os.Args))))
-	//		log.Fatalf("echo Arguments error")
-	//	}
-	//	for i := 0; i < len(os.Args)-1; i++ {
-	//		os.Args[i] = os.Args[i+1]
-	//	}
-	//} else {
-	//	if len(os.Args) != 6 && len(os.Args) != 2 {
-	//		fmt.Println("echo " + string(rune(len(os.Args))))
-	//		log.Fatalf("echo Arguments error")
-	//	}
-	//}
-	//method := os.Args[0]
-	//switch method {
-	//case "start":
-	//	start(os.Args[1], os.Args[2], os.Args[3], os.Args[4], os.Args[5])
-	//	break
-	//case "stop":
-	//	stop(os.Args[1])
-	//	break
-	//case "status":
-	//	status(os.Args[1])
-	//	break
-	//default:
-	//	log.Fatalf("echo Invalid method used: %s", method)
-	//}
 	_ = godotenv.Load(".env")
 
 	var config Config
