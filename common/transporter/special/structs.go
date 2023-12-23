998018556aed49955f3eaf54a88241112175c918
Liam Burnand <liam.burnand@ystv.co.uk>
2023-12-23 00:04:19 +0000

    Adding json tags

--- common/transporter/special/structs.go
+++ common/transporter/special/structs.go
@@ -6,8 +6,8 @@ type (
 	// InitiationTransporter allows for both the server type and the version to be communicated to the server
 	InitiationTransporter struct {
 		// Server is the server type
-		Server server.Server
+		Server server.Server `json:"server"`
 		// Version is the string of the version
-		Version string
+		Version string `json:"version"`
 	}
 )
