87b149caab979b4d30de229ce254ff6140ed26eb
Liam Burnand <liam.burnand@ystv.co.uk>
2023-12-25 13:20:53 +0000

    Fixed to specify returns instead of assumed

--- server/views/handleTXLight.go
+++ server/views/handleTXLight.go
@@ -8,35 +8,36 @@ import (
 	"github.com/ystv/streamer/server/helper/tx"
 )
 
-func (v *Views) HandleTXLight(url string, function tx.FunctionTX) (err error) {
+func (v *Views) HandleTXLight(url string, function tx.FunctionTX) error {
 	switch function {
 	case tx.TransmissionOn:
-		_, err = http.Get(url + tx.TransmissionOn.String())
+		_, err := http.Get(url + tx.TransmissionOn.String())
 		if err != nil && !strings.Contains(err.Error(), "unexpected EOF") {
 			return fmt.Errorf("failed to get response from tx light transmission on: %w", err)
 		}
-		return
+		return nil
 	case tx.AllOff:
 		if !v.ExistingStreamCheck() {
-			_, err = http.Get(url + tx.AllOff.String()) // Output is ignored as it returns a 204 status and there's a weird bug with no content
+			_, err := http.Get(url + tx.AllOff.String()) // Output is ignored as it returns a 204 status
+			// and there's a unique bug with no content
 			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") {
 				return fmt.Errorf("failed to get response from tx light all off: %w", err)
 			}
 		} else if !v.SavedStreamCheck() {
-			_, err = http.Get(url + tx.RehearsalOn.String())
+			_, err := http.Get(url + tx.RehearsalOn.String())
 			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") {
 				return fmt.Errorf("failed to get response from tx light rehearsal on: %w", err)
 			}
 		}
-		return
+		return nil
 	case tx.RehearsalOn:
 		if !v.ActiveStreamCheck() {
-			_, err = http.Get(url + tx.RehearsalOn.String())
+			_, err := http.Get(url + tx.RehearsalOn.String())
 			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") {
 				return fmt.Errorf("failed to get response from tx light rehearsal on: %w", err)
 			}
 		}
-		return
+		return nil
 	default:
 		return fmt.Errorf("unexpected function string: \"%s\"", function)
 	}
