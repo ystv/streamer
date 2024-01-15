32a2dbf947f9544bab8c92bd5944cc24046f7d20
Liam Burnand <liam.burnand@ystv.co.uk>
2024-01-15 20:12:27 +0000

    Testing new status page, only testing on the first status page

--- server/views/status.go
+++ server/views/status.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 	"sync"
 
 	"github.com/labstack/echo/v4"
@@ -18,6 +17,19 @@ import (
 	"github.com/ystv/streamer/common/wsMessages"
 )
 
+type (
+	StatusResponse struct {
+		Status []StatusResponseIndividual `json:"status"`
+		Error  string                     `json:"error"`
+	}
+
+	StatusResponseIndividual struct {
+		Name     string `json:"name"`
+		Response string `json:"response"`
+		Error    string `json:"error"`
+	}
+)
+
 // StatusFunc is used to check the status of the streams and does this by tail command of the output logs
 func (v *Views) StatusFunc(c echo.Context) error {
 	if c.Request().Method == "POST" {
@@ -25,18 +37,28 @@ func (v *Views) StatusFunc(c echo.Context) error {
 			log.Println("Status POST called")
 		}
 
+		errResponse := struct {
+			Error string `json:"error"`
+		}{}
+
 		unique := c.FormValue("unique_code")
 		if len(unique) != 10 {
-			return fmt.Errorf("unique key invalid: %s", unique)
+			log.Printf("unique key invalid: %s", unique)
+			errResponse.Error = fmt.Sprintf("unique key invalid: %s", unique)
+			return c.JSON(http.StatusOK, errResponse)
 		}
 
 		stream, err := v.store.FindStream(unique)
 		if err != nil {
-			return fmt.Errorf("unable to find stream for status: %s, %w", unique, err)
+			log.Printf("unable to find stream for status: %s, %+v", unique, err)
+			errResponse.Error = fmt.Sprintf("unable to find stream for status: %s, %+v", unique, err)
+			return c.JSON(http.StatusOK, errResponse)
 		}
 
 		if stream == nil {
-			return fmt.Errorf("failed to get stream as data is empty")
+			log.Println("failed to get stream as data is empty")
+			errResponse.Error = fmt.Sprintf("failed to get stream as data is empty")
+			return c.JSON(http.StatusOK, errResponse)
 		}
 
 		transporter := commonTransporter.Transporter{
@@ -50,7 +72,8 @@ func (v *Views) StatusFunc(c echo.Context) error {
 			Streams: int(stream.Streams),
 		}
 
-		m := make(map[string]string)
+		var statusResponse StatusResponse
+		//m := make(map[string]string)
 		var wg sync.WaitGroup
 		if stream.Recording {
 			wg.Add(2)
@@ -58,24 +81,37 @@ func (v *Views) StatusFunc(c echo.Context) error {
 				defer wg.Done()
 				recorderTransporter := transporter
 
+				individualResponse := StatusResponseIndividual{
+					Name: "recording",
+				}
+
 				var response commonTransporter.ResponseTransporter
 				response, err = v.wsHelper(server.Recorder, recorderTransporter)
 				if err != nil {
 					log.Printf("failed to send or receive message from recorder for status: %+v", err)
-					m["recording"] = fmt.Sprintf("failed to send or receive message from recorder for status: %+v", err)
+					individualResponse.Error = fmt.Sprintf("failed to send or receive message from recorder for status: %+v", err)
+					statusResponse.Status = append(statusResponse.Status, individualResponse)
+					//m["recording"] = fmt.Sprintf("failed to send or receive message from recorder for status: %+v", err)
 					return
 				}
 				if response.Status == wsMessages.Error {
 					log.Printf("failed to get correct response from recorder for status: %s", response.Payload)
-					m["recording"] = fmt.Sprintf("failed to get correct response from recorder for status: %s", response.Payload)
+					//m["recording"] = fmt.Sprintf("failed to get correct response from recorder for status: %s", response.Payload)
+					individualResponse.Error = fmt.Sprintf("failed to get correct response from recorder for status: %s", response.Payload)
+					statusResponse.Status = append(statusResponse.Status, individualResponse)
 					return
 				}
 				if response.Status != wsMessages.Okay {
 					log.Printf("invalid response from recorder for status: %s", response)
-					m["recording"] = fmt.Sprintf("invalid response from recorder for status: %s", response)
+					individualResponse.Error = fmt.Sprintf("invalid response from recorder for status: %s", response)
+					statusResponse.Status = append(statusResponse.Status, individualResponse)
+					//m["recording"] = fmt.Sprintf("invalid response from recorder for status: %s", response)
 					return
 				}
-				m["recording"] = response.Payload.(string)
+				//m["recording"] = response.Payload.(string)
+
+				individualResponse.Response = response.Payload.(string)
+				statusResponse.Status = append(statusResponse.Status, individualResponse)
 
 				log.Println("Recorder status success")
 			}()
@@ -88,21 +124,31 @@ func (v *Views) StatusFunc(c echo.Context) error {
 
 			forwarderTransporter.Payload = fStatus
 
+			individualErrResponse := StatusResponseIndividual{
+				Name: "1",
+			}
+
 			var response commonTransporter.ResponseTransporter
 			response, err = v.wsHelper(server.Forwarder, forwarderTransporter)
 			if err != nil {
 				log.Printf("failed to send or receive message from forwarder for status: %+v", err)
-				m["0"] = fmt.Sprintf("failed to send or receive message from forwarder for status: %+v", err)
+				individualErrResponse.Error = fmt.Sprintf("failed to send or receive message from forwarder for status: %+v", err)
+				statusResponse.Status = append(statusResponse.Status, individualErrResponse)
+				//m["0"] = fmt.Sprintf("failed to send or receive message from forwarder for status: %+v", err)
 				return
 			}
 			if response.Status == wsMessages.Error {
 				log.Printf("failed to get correct response from forwarder for status: %s", response.Payload)
-				m["0"] = fmt.Sprintf("failed to get correct response from forwarder for status: %s", response.Payload)
+				individualErrResponse.Error = fmt.Sprintf("failed to get correct response from forwarder for status: %s", response.Payload)
+				statusResponse.Status = append(statusResponse.Status, individualErrResponse)
+				//m["0"] = fmt.Sprintf("failed to get correct response from forwarder for status: %s", response.Payload)
 				return
 			}
 			if response.Status != wsMessages.Okay {
 				log.Printf("invalid response from recorder for status: %s", response)
-				m["0"] = fmt.Sprintf("invalid response from recorder for status: %s", response)
+				individualErrResponse.Error = fmt.Sprintf("invalid response from recorder for status: %s", response)
+				statusResponse.Status = append(statusResponse.Status, individualErrResponse)
+				//m["0"] = fmt.Sprintf("invalid response from recorder for status: %s", response)
 				return
 			}
 
@@ -111,90 +157,60 @@ func (v *Views) StatusFunc(c echo.Context) error {
 			err = mapstructure.Decode(response.Payload, &forwarderStatus)
 			if err != nil {
 				log.Printf("failed to decode message from forwarder for status: %+v", err)
-				m["0"] = fmt.Sprintf("failed to decode message from forwarder for status: %+v", err)
+				individualErrResponse.Error = fmt.Sprintf("failed to decode message from forwarder for status: %+v", err)
+				statusResponse.Status = append(statusResponse.Status, individualErrResponse)
+				//m["0"] = fmt.Sprintf("failed to decode message from forwarder for status: %+v", err)
 				return
 			}
 
 			if len(forwarderStatus.Website) > 0 {
-				m["website"] = forwarderStatus.Website
+				individualResponse := StatusResponseIndividual{
+					Name:     "website",
+					Response: response.Payload.(string),
+				}
+				statusResponse.Status = append(statusResponse.Status, individualResponse)
+				//m["website"] = forwarderStatus.Website
 			}
 
 			for index, streamOut := range forwarderStatus.Streams {
-				m[strconv.Itoa(int(index))] = streamOut
+				individualResponse := StatusResponseIndividual{
+					Name:     strconv.Itoa(int(index)),
+					Response: streamOut,
+				}
+				statusResponse.Status = append(statusResponse.Status, individualResponse)
+				//m[strconv.Itoa(int(index))] = streamOut
 			}
-
-			//var client *ssh.Client
-			//var session *ssh.Session
-			//var err error
-			//if forwarderAuth == "PEM" {
-			//	client, session, err = connectToHostPEM(forwarder, forwarderUsername, forwarderPrivateKey, forwarderPassphrase)
-			//} else if forwarderAuth == "PASS" {
-			//client, session, err = helper.ConnectToHostPassword(v.conf.Forwarder, v.conf.ForwarderUsername, v.conf.ForwarderPassword, v.conf.Verbose)
-			//}
-			//if err != nil {
-			//	fmt.Println("Error connecting to Forwarder for status")
-			//	fmt.Println(err)
-			//}
-			//var dataOut []byte
-			//dataOut, err = session.CombinedOutput("./forwarder_status " + strconv.FormatBool(stream.Website) + " " + strconv.FormatUint(stream.Streams, 10) + " " + c.FormValue("unique"))
-			//if err != nil {
-			//	fmt.Println("Error executing on Forwarder for status")
-			//	fmt.Println(err)
-			//}
-			//err = client.Close()
-			//if err != nil {
-			//	fmt.Println(err)
-			//}
-			//
-			//dataOut1 := string(dataOut)[4 : len(dataOut)-2]
-			//
-			//dataOut2 := strings.Split(dataOut1, "\u0000")
-			//
-			//for _, dataOut3 := range dataOut2 {
-			//	if len(dataOut3) > 0 {
-			//		if strings.Contains(dataOut3, "frame=") {
-			//			dataOut4 := strings.Split(dataOut3, "~:~")
-			//			first := strings.Index(dataOut4[1], "frame=") - 1
-			//			last := strings.LastIndex(dataOut4[1], "\r")
-			//			dataOut4[1] = dataOut4[1][:last]
-			//			last = strings.LastIndex(dataOut4[1], "\r") + 1
-			//			if len(dataOut4) > 1 {
-			//				m[strings.Trim(dataOut4[0], " ")] = dataOut4[1][:first] + "\n" + dataOut4[1][last:]
-			//			} else {
-			//				fmt.Println(dataOut4)
-			//				m[strings.Trim(dataOut4[0], " ")] = ""
-			//			}
-			//		} else {
-			//			dataOut4 := strings.Split(dataOut3, "~:~")
-			//			if len(dataOut4) > 1 {
-			//				m[strings.Trim(dataOut4[0], " ")] = dataOut4[1]
-			//			} else {
-			//				fmt.Println(dataOut4)
-			//				m[strings.Trim(dataOut4[0], " ")] = ""
-			//			}
-			//		}
-			//	}
-			//}
 
 			log.Println("Forwarder status success")
 		}()
 		wg.Wait()
 		var jsonStr []byte
-		jsonStr, err = json.Marshal(m)
+		//jsonStr, err = json.Marshal(m)
+		//if err != nil {
+		//	log.Printf("failed to marshal response for status: %+v", err)
+		//	errResponse.Error = fmt.Sprintf("failed to marshal response for status: %+v", err)
+		//	return c.JSON(http.StatusOK, errResponse)
+		//}
+
+		jsonStr, err = json.Marshal(statusResponse)
 		if err != nil {
-			return fmt.Errorf("failed to marshal response for status: %w", err)
+			log.Printf("failed to marshal response for status: %+v", err)
+			errResponse.Error = fmt.Sprintf("failed to marshal response for status: %+v", err)
+			return c.JSON(http.StatusOK, errResponse)
 		}
-		output := strings.ReplaceAll(
-			strings.ReplaceAll(
-				strings.ReplaceAll(
-					strings.ReplaceAll(
-						strings.ReplaceAll(
-							string(jsonStr[1:len(jsonStr)-1]), "\\n", "<br>"),
-						"\"", ""),
-					" , ", "<br><br><br>"),
-				" ,", "<br><br><br>"),
-			"<br>,", "<br><br>")
-		return c.String(http.StatusOK, output)
+
+		//output := strings.ReplaceAll(
+		//	strings.ReplaceAll(
+		//		strings.ReplaceAll(
+		//			strings.ReplaceAll(
+		//				strings.ReplaceAll(
+		//					string(jsonStr[1:len(jsonStr)-1]), "\\n", "<br>"),
+		//				"\"", ""),
+		//			" , ", "<br><br><br>"),
+		//		" ,", "<br><br><br>"),
+		//	"<br>,", "<br><br>")
+		//return c.String(http.StatusOK, output)
+		return c.JSON(http.StatusOK, jsonStr)
 	}
 	return echo.NewHTTPError(http.StatusMethodNotAllowed, "invalid method")
 }
