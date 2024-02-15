a8c03f2c1eb8cb6491a090e33776d4e419155d08
Liam Burnand <liam.burnand@ystv.co.uk>
2024-02-15 01:22:49 +0000

    Adding temporary logging

--- forwarder/forwarder_stop.go
+++ forwarder/forwarder_stop.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strings"
 
 	commonTransporter "github.com/ystv/streamer/common/transporter"
@@ -9,8 +10,11 @@ import (
 
 func (v *Views) stop(transporter commonTransporter.Transporter) error {
 	found := false
+	log.Println(transporter.Unique)
 	for k, item := range v.cache.Items() {
+		log.Println(k)
 		if strings.Contains(k, transporter.Unique) && strings.Contains(k, finishChannelNameAppend) {
+			log.Println("Found")
 			found = true
 			close(item.Object.(chan bool))
 			v.cache.Delete(k)
