8ab5e01ab1d68e183ac2da4f2b3c175996395fca
Liam Burnand <liam.burnand@ystv.co.uk>
2024-02-16 20:14:39 +0000

    Cleaning up the testing logs

--- forwarder/forwarder_status.go
+++ forwarder/forwarder_status.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"os/exec"
 	"strings"
 
@@ -19,55 +18,27 @@ func (v *Views) status(transporter commonTransporter.Transporter) (commonTranspo
 
 	fStatusResponse := commonTransporter.ForwarderStatusResponse{}
 	fStatusResponse.Streams = make(map[string]string)
-	log.Println(10)
 
 	for i := start; i <= transporter.Payload.(commonTransporter.ForwarderStatus).Streams; i++ {
-		log.Println("i", i)
-		//c := exec.Command("tail", "-n", "19", fmt.Sprintf("\"logs/%s_%d.txt\"", transporter.Unique, i), "|", "sed", "-e", "\"s/\r$//\"")
-
-		c1 := exec.Command("tail", "-n", "19", fmt.Sprintf("/logs/%s_%d.txt", transporter.Unique, i))
-		//c2 := exec.Command("sed", "-e", "'s/\r$//'")
+		cmd := exec.Command("tail", "-n", "19", fmt.Sprintf("/logs/%s_%d.txt", transporter.Unique, i))
 
 		var stdout, stderr bytes.Buffer
-		//c2.Stdout = &stdout
-		//c2.Stderr = &stderr
-		c1.Stdout = &stdout
-		c1.Stderr = &stderr
-		log.Println(11)
-
-		//r, w := io.Pipe()
-		//c1.Stdout = w
-		//c2.Stdin = r
-
-		log.Println(12)
-
-		//_ = c1.Start()
-		//_ = c2.Start()
-		//_ = c1.Wait()
-		//_ = w.Close()
-		//_ = c2.Wait()
+		cmd.Stdout = &stdout
+		cmd.Stderr = &stderr
 
 		var errOut string
 
-		err := c1.Run()
+		err := cmd.Run()
 		if err != nil {
 			errOut = fmt.Sprintf("could not run command: %+v", err)
 		}
 
-		log.Println(13)
-		//stderr, err := c.StderrPipe()
 		errOut += stderr.String()
-		//scanner := bufio.NewScanner(stderr)
-		//for scanner.Scan() {
-		//	errOut += "\n" + scanner.Text()
-		//}
-		log.Println(14)
 
 		if len(errOut) != 0 {
 			return commonTransporter.ForwarderStatusResponse{}, fmt.Errorf(errOut)
 		}
 
-		log.Println(15)
 		var response string
 		tempRespArr := strings.Split(strings.TrimRight(stdout.String(), "\r"), "\r")
 		if len(tempRespArr) == 0 {
@@ -82,10 +53,7 @@ func (v *Views) status(transporter commonTransporter.Transporter) (commonTranspo
 		} else {
 			fStatusResponse.Streams[fmt.Sprintf("%d", i)] = response
 		}
-		log.Println(16)
 	}
-	log.Println(17)
-	log.Printf("%#v", fStatusResponse)
 
 	return fStatusResponse, nil
 }
