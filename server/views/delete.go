b6c9804841c51a1afa61c420ce34f48991910372
Liam Burnand <liam.burnand@ystv.co.uk>
2024-02-17 16:14:10 +0000

    Returning json for deleted

--- server/views/delete.go
+++ server/views/delete.go
@@ -2,10 +2,9 @@ package views
 
 import (
 	"fmt"
+	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
-
-	"github.com/labstack/echo/v4"
 )
 
 // DeleteFunc will delete the saved stream before it can start
@@ -15,27 +14,42 @@ func (v *Views) DeleteFunc(c echo.Context) error {
 			log.Println("Delete POST called")
 		}
 
+		var response struct {
+			Deleted bool   `json:"deleted"`
+			Error   string `json:"error"`
+		}
+
 		unique := c.FormValue("unique")
+		if len(unique) != 10 {
+			log.Printf("unique key invalid: %s", unique)
+			response.Error = fmt.Sprintf("unique key invalid: %s", unique)
+			return c.JSON(http.StatusOK, response)
+		}
 
 		stored, err := v.store.FindStored(unique)
 		if err != nil {
-			return fmt.Errorf("failed to get stored: %w, unique: %s", err, unique)
+			log.Printf("unable to find unique code for delete: %s, %+v", unique, err)
+			response.Error = fmt.Sprintf("unable to find unique code for delete: %s, %+v", unique, err)
+			return c.JSON(http.StatusOK, response)
 		}
 
 		if stored == nil {
-			log.Println("no data in stored")
-			log.Printf("rejected delete: %s", unique)
-			return c.String(http.StatusOK, "REJECTED!")
+			log.Printf("failed to get stored as data is empty")
+			response.Error = "failed to get stored as data is empty"
+			return c.JSON(http.StatusOK, response)
 		}
 
 		err = v.store.DeleteStream(unique)
 		if err != nil {
 			log.Printf("failed to delete stored: %+v, unique: %s", err, unique)
-			return c.String(http.StatusOK, "REJECTED!")
+			response.Error = fmt.Sprintf("failed to delete stored: %+v, unique: %s", err, unique)
+			return c.JSON(http.StatusOK, response)
 		}
 
 		log.Printf("deleted stored: %s", unique)
-		return c.String(http.StatusOK, "DELETED!")
+
+		response.Deleted = true
+		return c.JSON(http.StatusOK, response)
 	}
 	return echo.NewHTTPError(http.StatusMethodNotAllowed, "invalid method")
 }
