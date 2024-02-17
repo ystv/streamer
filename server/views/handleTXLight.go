89bd538021531afceb702289d859e526ad1e8820
Liam Burnand <liam.burnand@ystv.co.uk>
2024-02-17 14:58:00 +0000

    Improving the tx light handler

--- server/views/handleTXLight.go
+++ server/views/handleTXLight.go
@@ -9,36 +9,39 @@ import (
 )
 
 func (v *Views) HandleTXLight(url string, function tx.FunctionTX) error {
+	var resp *http.Response
+	var err error
 	switch function {
 	case tx.TransmissionOn:
-		_, err := http.Get(url + tx.TransmissionOn.String())
-		if err != nil && !strings.Contains(err.Error(), "unexpected EOF") {
+		resp, err = http.Get(url + tx.TransmissionOn.String())
+		if err != nil && !strings.Contains(err.Error(), "unexpected EOF") && (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent) {
 			return fmt.Errorf("failed to get response from tx light transmission on: %w", err)
 		}
-		return nil
+		break
 	case tx.AllOff:
 		if !v.ExistingStreamCheck() {
-			_, err := http.Get(url + tx.AllOff.String()) // Output is ignored as it returns a 204 status
-			// and there's a unique bug with no content
-			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") {
+			resp, err = http.Get(url + tx.AllOff.String())
+			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") && (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent) {
 				return fmt.Errorf("failed to get response from tx light all off: %w", err)
 			}
 		} else if !v.SavedStreamCheck() {
-			_, err := http.Get(url + tx.RehearsalOn.String())
-			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") {
+			resp, err = http.Get(url + tx.RehearsalOn.String())
+			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") && (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent) {
 				return fmt.Errorf("failed to get response from tx light rehearsal on: %w", err)
 			}
 		}
-		return nil
+		break
 	case tx.RehearsalOn:
 		if !v.ActiveStreamCheck() {
-			_, err := http.Get(url + tx.RehearsalOn.String())
-			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") {
+			resp, err = http.Get(url + tx.RehearsalOn.String())
+			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") && (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent) {
 				return fmt.Errorf("failed to get response from tx light rehearsal on: %w", err)
 			}
 		}
-		return nil
+		break
 	default:
 		return fmt.Errorf("unexpected function string: \"%s\"", function)
 	}
+
+	return nil
 }
