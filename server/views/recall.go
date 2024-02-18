91d3bf26571100cddef1366517639c63a73f4472
Liam Burnand <liam.burnand@ystv.co.uk>
2024-02-18 16:13:48 +0000

    Fixing the recalling to use json

--- server/views/recall.go
+++ server/views/recall.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 
 	"github.com/ystv/streamer/server/templates"
 )
+
+type RecallStream struct {
+	StreamServer string `json:"streamServer"`
+	StreamKey    string `json:"streamKey"`
+}
 
 // RecallFunc can pull back up stream details from the save function and allows you to start a stored stream
 func (v *Views) RecallFunc(c echo.Context) error {
@@ -23,32 +29,71 @@ func (v *Views) RecallFunc(c echo.Context) error {
 			log.Println("Recall POST called")
 		}
 
-		errResponse := struct {
-			Error string `json:"error"`
+		response := struct {
+			Unique        string         `json:"unique"`
+			InputEndpoint string         `json:"inputEndpoint"`
+			InputStream   string         `json:"inputStream"`
+			RecordingPath string         `json:"recordingPath,omitempty"`
+			WebsiteStream string         `json:"websiteStream,omitempty"`
+			Streams       []RecallStream `json:"streams"`
+			Error         string         `json:"error"`
 		}{}
 
 		unique := c.FormValue("unique")
 		if len(unique) != 10 {
 			log.Printf("unique key invalid: %s", unique)
-			errResponse.Error = fmt.Sprintf("unique key invalid: %s", unique)
-			return c.JSON(http.StatusOK, errResponse)
+			response.Error = fmt.Sprintf("unique key invalid: %s", unique)
+			return c.JSON(http.StatusOK, response)
 		}
 
 		stored, err := v.store.FindStored(unique)
 		if err != nil {
 			log.Printf("failed to get stored: %+v, unique: %s", err, unique)
-			errResponse.Error = fmt.Sprintf("failed to get stored: %+v, unique: %s", err, unique)
-			return c.JSON(http.StatusOK, errResponse)
+			response.Error = fmt.Sprintf("failed to get stored: %+v, unique: %s", err, unique)
+			return c.JSON(http.StatusOK, response)
 		}
 
 		if stored == nil {
-			log.Println("No data")
-			log.Printf("rejected recall: %s", unique)
-			return c.String(http.StatusOK, "REJECTED!")
+			log.Printf("failed to get stored as data is empty")
+			response.Error = "failed to get stored as data is empty"
+			return c.JSON(http.StatusOK, response)
+		}
+
+		response.Unique = stored.Stream
+
+		if len(stored.Recording) > 0 {
+			response.RecordingPath = stored.Recording
+		}
+
+		if len(stored.Website) > 0 {
+			response.WebsiteStream = stored.Website
+		}
+
+		inputPart := strings.Split(stored.Input, "/")
+		if len(inputPart) != 2 {
+			log.Printf("failed to get input stream string, invalid array size: %d, %+v", len(inputPart), inputPart)
+			response.Error = fmt.Sprintf("failed to get input stream string, invalid array size: %d, %+v", len(inputPart), inputPart)
+			return c.JSON(http.StatusOK, response)
+		}
+		response.InputEndpoint = inputPart[0]
+		response.InputStream = inputPart[1]
+
+		response.Streams = []RecallStream{}
+		for _, stream := range stored.Streams {
+			var recallStream RecallStream
+			splitStream := strings.Split(stream, "|")
+			if len(splitStream) != 2 {
+				log.Printf("failed to get output stream string, invalid array size: %d, %+v", len(splitStream), splitStream)
+				response.Error = fmt.Sprintf("failed to get output stream string, invalid array size: %d, %+v", len(splitStream), splitStream)
+				return c.JSON(http.StatusOK, response)
+			}
+			recallStream.StreamServer = splitStream[0]
+			recallStream.StreamKey = splitStream[1]
+			response.Streams = append(response.Streams, recallStream)
 		}
 
 		log.Printf("accepted recall: %s", unique)
-		return c.String(http.StatusOK, "ACCEPTED!")
+		return c.JSON(http.StatusOK, response)
 	}
 	return echo.NewHTTPError(http.StatusMethodNotAllowed, "invalid method")
 }
