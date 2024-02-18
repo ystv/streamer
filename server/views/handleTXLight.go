a53e895d447f51e543096ca1b021cd007dd2e78b
Liam Burnand <liam.burnand@ystv.co.uk>
2024-02-18 16:51:57 +0000

    I will revisit this later

--- server/views/handleTXLight.go
+++ server/views/handleTXLight.go
@@ -2,7 +2,6 @@ package views
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strings"
 
@@ -15,32 +14,33 @@ func (v *Views) HandleTXLight(url string, function tx.FunctionTX) error {
 	switch function {
 	case tx.TransmissionOn:
 		resp, err = http.Get(url + tx.TransmissionOn.String())
-		if err != nil && !strings.Contains(err.Error(), "unexpected EOF") && (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent) {
+		if err != nil && !strings.Contains(err.Error(), "unexpected EOF") /*&& (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent)*/ {
 			return fmt.Errorf("failed to get response from tx light transmission on: %w", err)
 		}
 	case tx.AllOff:
 		if !v.ExistingStreamCheck() {
 			resp, err = http.Get(url + tx.AllOff.String())
-			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") && (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent) {
+			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") /*&& (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent)*/ {
 				return fmt.Errorf("failed to get response from tx light all off: %w", err)
 			}
 		} else if !v.SavedStreamCheck() {
 			resp, err = http.Get(url + tx.RehearsalOn.String())
-			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") && (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent) {
+			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") /*&& (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent)*/ {
 				return fmt.Errorf("failed to get response from tx light rehearsal on: %w", err)
 			}
 		}
 	case tx.RehearsalOn:
 		if !v.ActiveStreamCheck() {
 			resp, err = http.Get(url + tx.RehearsalOn.String())
-			log.Printf("response: %#v, error: %#v", resp, err)
-			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") && (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent) {
+			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") /*&& (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent)*/ {
 				return fmt.Errorf("failed to get response from tx light rehearsal on: %w", err)
 			}
 		}
 	default:
 		return fmt.Errorf("unexpected function string: \"%s\"", function)
 	}
+
+	_ = resp
 
 	return nil
 }
