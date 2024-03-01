5b7778900fca5d044714ba6e3ce5ca0fbc4841d5
Liam Burnand <liam.burnand@ystv.co.uk>
2024-03-01 00:17:22 +0000

    Adding a 2-second timeout as it should be fast

--- server/views/handleTXLight.go
+++ server/views/handleTXLight.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/ystv/streamer/server/helper/tx"
 )
@@ -11,27 +12,30 @@ import (
 func (v *Views) HandleTXLight(url string, function tx.FunctionTX) error {
 	var resp *http.Response
 	var err error
+	client := http.Client{
+		Timeout: 2 * time.Second,
+	}
 	switch function {
 	case tx.TransmissionOn:
-		resp, err = http.Get(url + tx.TransmissionOn.String())
+		resp, err = client.Get(url + tx.TransmissionOn.String())
 		if err != nil && !strings.Contains(err.Error(), "unexpected EOF") /*&& (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent)*/ {
 			return fmt.Errorf("failed to get response from tx light transmission on: %w", err)
 		}
 	case tx.AllOff:
 		if !v.ExistingStreamCheck() {
-			resp, err = http.Get(url + tx.AllOff.String())
+			resp, err = client.Get(url + tx.AllOff.String())
 			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") /*&& (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent)*/ {
 				return fmt.Errorf("failed to get response from tx light all off: %w", err)
 			}
 		} else if !v.SavedStreamCheck() {
-			resp, err = http.Get(url + tx.RehearsalOn.String())
+			resp, err = client.Get(url + tx.RehearsalOn.String())
 			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") /*&& (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent)*/ {
 				return fmt.Errorf("failed to get response from tx light rehearsal on: %w", err)
 			}
 		}
 	case tx.RehearsalOn:
 		if !v.ActiveStreamCheck() {
-			resp, err = http.Get(url + tx.RehearsalOn.String())
+			resp, err = client.Get(url + tx.RehearsalOn.String())
 			if err != nil && !strings.Contains(err.Error(), "unexpected EOF") /*&& (resp.StatusCode < http.StatusOK || resp.StatusCode > http.StatusNoContent)*/ {
 				return fmt.Errorf("failed to get response from tx light rehearsal on: %w", err)
 			}
