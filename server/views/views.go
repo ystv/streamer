c90e8bcfaff970e7d064a8739cf1244cb70ba768
Liam Burnand <liam.burnand@ystv.co.uk>
2025-01-06 07:18:30 +0000

    Adding more debugging again

--- server/views/views.go
+++ server/views/views.go
@@ -264,16 +264,19 @@ func (v *Views) Authenticated(next echo.HandlerFunc) echo.HandlerFunc {
 			log.Printf("failed to create new get token request: %+v", err)
 			goto login
 		}
+		fmt.Println(2.1)
 
 		for _, cookie := range c.Request().Cookies() {
 			req.AddCookie(cookie)
 		}
+		fmt.Println(2.2)
 
 		resp, err = client.Do(req)
 		if err != nil {
 			log.Printf("failed to do client for get token: %+v", err)
 			goto login
 		}
+		fmt.Println(2.3)
 		defer resp.Body.Close()
 
 		b, err = io.ReadAll(resp.Body)
@@ -281,24 +284,28 @@ func (v *Views) Authenticated(next echo.HandlerFunc) echo.HandlerFunc {
 			log.Printf("failed to read get token body: %+v", err)
 			goto login
 		}
+		fmt.Println(2.4)
 
 		err = json.Unmarshal(b, &t)
 		if err != nil {
 			log.Printf("failed to unmarshal JSON for get token: %+v", err)
 			goto login
 		}
+		fmt.Println(2.5)
 
 		fmt.Println(t)
 
 		if t.Token == "" {
 			goto login
 		}
+		fmt.Println(2.6)
 
 		fmt.Println(resp.StatusCode)
 
 		if resp.StatusCode != 201 {
 			goto login
 		}
+		fmt.Println(2.7)
 
 		session.Values["token"] = t.Token
 
@@ -307,6 +314,7 @@ func (v *Views) Authenticated(next echo.HandlerFunc) echo.HandlerFunc {
 			log.Printf("failed to save token session for authentication: %+v", err)
 			goto login
 		}
+		fmt.Println(2.8)
 		return next(c)
 
 	login:
